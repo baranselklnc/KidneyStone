cb3283884eedf7acdcb6572bcc2dd5d4797b6548
Baransel <baranselklnc@gmail.com>
2024-12-12 00:55:03 +0300

    evaluateRisk function analysed  kindsey stone for elements

--- app/src/main/java/com/example/kidneystone/network/ProductResponse.kt
+++ app/src/main/java/com/example/kidneystone/network/ProductResponse.kt
@@ -2,10 +2,35 @@ package com.example.kidneystone.network
 
 data class ProductResponse(
     val status: Int,
-    val product: Product?
-)
+    val product: Product?,
+    val isRisky: Boolean,
+
+){
+    fun evaluateRisk(): Boolean {
+        // Kategoriye göre analiz
+        if (product?.categories_tags?.contains("tomato-products") == true) {
+            return true // Oksalat riski
+        }
+
+        // Alternatif kategori kontrolü
+        if (product?.categories?.contains("Tomato") == true) {
+            return true // Oksalat riski
+        }
+
+        return false // Risk bulunamadı
+    }
+
+}
 
 data class Product(
     val product_name: String?,
-    val ingredients_text: String?
+    val ingredients_text: String?,
+    val nutriments: Nutriments?, // Eklenen alan
+    val categories: String?, // Kategorileri alır
+    val categories_tags: List<String>? // Kategori etiketlerini alır
+)
+
+data class Nutriments(
+    val sodium: Double?, // mg cinsinden
+    val calcium: Double? // mg cinsinden
 )
