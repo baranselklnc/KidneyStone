3d49b987164bab0c02fb282e0e8f57be15a5aeee
Baransel <baranselklnc@gmail.com>
2024-12-18 21:36:15 +0300

    created an onboarding screen algorithm if the user is installing the application for the first time

--- app/src/main/java/com/example/kidneystone/CtaFragment.kt
+++ app/src/main/java/com/example/kidneystone/CtaFragment.kt
@@ -1,5 +1,6 @@
 package com.example.kidneystone
 
+import android.content.Context
 import android.content.Intent
 import android.os.Bundle
 import androidx.fragment.app.Fragment
@@ -8,27 +9,28 @@ import android.view.View
 import android.view.ViewGroup
 import android.widget.Button
 
-
 class CtaFragment : Fragment() {
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
-    )
-    : View? {
-        // Inflate the layout for this fragment
-        val view= inflater.inflate(R.layout.fragment_cta, container, false)
+    ): View? {
+        val view = inflater.inflate(R.layout.fragment_cta, container, false)
 
-        val startAppButton=view.findViewById<Button>(R.id.btnStartApp)
+        val startAppButton = view.findViewById<Button>(R.id.btnStartApp)
         startAppButton.setOnClickListener {
-            val intent=Intent(requireContext(),MainActivity::class.java)
+            // Onboarding tamamlandı bilgisini kaydet
+            val sharedPref = requireActivity().getSharedPreferences("Onboarding", Context.MODE_PRIVATE)
+            sharedPref.edit().putBoolean("isOnboardingCompleted", true).apply()
+
+            // Ana ekrana yönlendir
+            val intent = Intent(requireContext(), MainActivity::class.java)
             startActivity(intent)
 
+            // OnboardingActivity'yi kapat
             requireActivity().finish()
         }
 
-
         return view
-
-}
     }
+}
