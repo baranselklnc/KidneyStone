eab28e5eb04dc0fa8f19ebb026e55855e7bfa510
Baransel <baranselklnc@gmail.com>
2024-12-19 00:42:27 +0300

    animation set and play setter

--- app/src/main/java/com/example/kidneystone/FeatureFragment.kt
+++ app/src/main/java/com/example/kidneystone/FeatureFragment.kt
@@ -5,6 +5,7 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import com.airbnb.lottie.LottieAnimationView
 
 class FeatureFragment : Fragment() {
 
@@ -16,5 +17,17 @@ class FeatureFragment : Fragment() {
     ): View? {
         return inflater.inflate(R.layout.fragment_feature, container, false)
     }
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+
+        val animationView= view.findViewById<LottieAnimationView>(R.id.lottie)
+        animationView.setAnimation(R.raw.barcode)
+        animationView.playAnimation()
+
+
+
+    }
+
 
 }
