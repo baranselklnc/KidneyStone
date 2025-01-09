d0b0fead504e894465e5c4dbd0eca2050cc6aa83
Baransel <baranselklnc@gmail.com>
2025-01-09 22:39:38 +0300

    lottie added onboarding

--- app/src/main/java/com/example/kidneystone/ui/onboarding/WelcomeFragment.kt
+++ app/src/main/java/com/example/kidneystone/ui/onboarding/WelcomeFragment.kt
@@ -5,6 +5,7 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import com.airbnb.lottie.LottieAnimationView
 
 class WelcomeFragment : Fragment() {
 
@@ -15,6 +16,20 @@ class WelcomeFragment : Fragment() {
     ): View? {
         // Inflate the layout for this fragment
         return inflater.inflate(R.layout.fragment_welcome, container, false)
+
+
+
+    }
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+
+        val animationView= view.findViewById<LottieAnimationView>(R.id.supermarket_anim)
+        animationView.setAnimation(R.raw.supermarket)
+        animationView.playAnimation()
+
+
+
     }
 
 }
