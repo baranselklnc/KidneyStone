8d7a4e522c3babdc67ed8e6b44ba035b775bc36d
Baransel <baranselklnc@gmail.com>
2025-01-13 22:54:12 +0300

    response class unused or wrong used term deleted

--- app/src/main/java/com/example/kidneystone/network/ProductResponse.kt
+++ app/src/main/java/com/example/kidneystone/network/ProductResponse.kt
@@ -2,22 +2,18 @@ package com.example.kidneystone.network
 
 data class ProductResponse(
     val status: Int,
-    val product: Product?,
-    val isRisky: Boolean,
-
+    val product: Product?
 )
-
-
 
 data class Product(
     val product_name: String?,
     val ingredients_text: String?,
-    val nutriments: Nutriments?, // Eklenen alan
-    val categories: String?, // Kategorileri alır
-    val categories_tags: List<String>? // Kategori etiketlerini alır
+    val nutriments: Nutriments?, // Nutriments verisi
+    val categories: String?, // Kategoriler
+    val categories_tags: List<String>? // Kategori etiketleri
 )
 
 data class Nutriments(
     val sodium_100g: Double?,
-    val calcium_100g: Double?,
+    val calcium_100g: Double?
 )
